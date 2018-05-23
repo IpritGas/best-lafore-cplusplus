116ae0cd6a11c55f27261f0df710d155883b9b61
Vladimir Savich <vl.a.savich@gmail.com>
2018-05-23 15:41:36 +0300

    Add files via upload

---
+++ 13.1/13.1.cpp
@@ -0,0 +1,40 @@
+// 13.1.cpp : Defines the entry point for the console application.
+// Verylong Сalculator
+
+#include "stdafx.h"
+#include "verylong.h"    //заголовочный файл verylong
+
+int main()
+{
+	char choise, sign;
+	verylong vl_1, vl_2, vl_r;
+	do
+	{
+		cout << "Enter the expression" << endl;
+		cout << "(first variable, operator, second variable):" << endl;
+		cin >> vl_1 >> sign >> vl_2;
+		switch (sign)
+		{
+		case '+':
+			vl_r = vl_1 + vl_2;
+			break;
+		case '-':
+			vl_r = vl_1 - vl_2;
+			break;
+		case '*':
+			vl_r = vl_1 * vl_2;
+			break;
+		case '/':
+			vl_r = vl_1 / vl_2;
+			break;
+		default:
+			cout << "Unknown operator!" << endl;
+			exit(1);
+		}
+		cout << "Result: " << vl_r << endl;
+		cout << "Another operation (y/n)? " << flush;
+		cin >> choise;
+	} while (choise != 'n');
+	system("pause");
+	return 0;
+}
